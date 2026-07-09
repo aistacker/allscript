41f849669d321eb733e7ebe5c4711e8fd5cce77c
aistacker <lionsvkr@gmail.com>
2026-07-09 16:50:38 +0000

    conversion of data type

---
+++ 01_BASICS/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score =undefined
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber);     //NaN
+
+
+
+// "33" ==>33
+//"33abc"==>NaN
+//true=>1; false=>0
+
+let isLoggedIn="1"
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);            //true
+//1==>true ;0=>false
+//vishal ==>true
+
+
+let someNumber=12
+let strinNumber=String(someNumber)
+console.log(strinNumber);
+console.log(typeof strinNumber);
+
