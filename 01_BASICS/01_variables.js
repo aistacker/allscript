1b2409cfc34d98d807464d3d671d45d92c58c8a5
aistacker <lionsvkr@gmail.com>
2026-07-09 16:32:00 +0000

    datatype

---
+++ 01_BASICS/01_variables.js
@@ -0,0 +1,29 @@
+const accountId = 1445634
+
+let accountEmail ="kjdfi@gmail.com"   /// use only let 
+var accountPasswod ="2345465"
+
+accountCity ="Patna"  
+
+let accountstate;
+
+console.log(accountstate);  // undefined
+
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope 
+*/
+
+//accountId =2  //not allowed 
+accountEmail ="dsdn@gmail.com"
+accountPasswod=456576
+accountCity ="BBSR"
+// console.log(accountId);
+
+console.log([accountEmail,accountPasswod,accountCity]);
+
+
+//{
+    //Scope
+//}
---
+++ 01_BASICS/02_dataTypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all code as newer version
+
+//alert(3+3) //we are using node js not broswer
+
+console.log(3+
+3); //COde readability shoud be high 
+
+console.log("vishal");
+
+
+let name ="vishal"
+let age =18
+let isLoggedIn=false
+
+let state =null 
+
+//number=>2 to 53
+//big int
+
+//string ---> use in double quotes ""
+
+//boolean ===>true/false  
+
+//null ==>standalone value  //output object 
+
+//undefined =>
+
+//symbol ==> unique 
+
+
+//object 
+
+
+console.log(typeof undefined);
+
+
+
+
+
