0efbca7d4c093f3f71c0debe0f941ba6ca14e2b1
aistacker <lionsvkr@gmail.com>
2026-07-12 08:37:44 +0000

    DATA TYPE SUMMARY

---
+++ 01_BASICS/datatypes-summary.js
@@ -0,0 +1,55 @@
+/// # premitive
+
+
+//7 types :   Striinig , Number ,Boolean, Null ,Undefiined  ,Symbol ,BigInt Call by value 
+
+const score =100 
+const scoreVlaue=100.3
+
+
+//const LoogedIn= False;
+const outsideTemp =null
+
+let userEmail;
+
+const id=Symbol('123')
+const anotherID= Symbol('123')
+
+//console.log(id==anotherID);
+
+const bigNumber=3464576854768675658n
+
+console.log(typeof(bigNumber));
+
+
+// Reference type   non-premitive
+
+// Array, objects ,Functions 
+
+
+
+const heros=["a","rrt","dfrg","fdgg","fdg"]
+
+
+let obj={
+    name:"vishal",
+    age: 27,
+}
+
+
+
+
+
+const myFunction = function(){
+
+    console.log("hello world");
+    
+}
+
+ console.log(typeof(myFunction));
+ 
+
+
+
+
+
