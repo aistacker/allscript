6c844d0c7db927f2cadfe3e004868fd8d264e7d6
aistacker <lionsvkr@gmail.com>
2026-07-16 16:09:26 +0000

    Num

---
+++ 01_BASICS/06_nums_and_math.js
@@ -0,0 +1,25 @@
+const score = 500
+console.log(score);
+
+
+
+const balance =new Number(400)
+console.log(balance);
+
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));   //Precision  value
+
+
+const otherNumber =23.8966
+console.log(otherNumber.toPrecision(3)); ///start from 23.9  
+
+
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
