10ac4c73c197eed6be4c0e8272990d7d65bda65e
aistacker <lionsvkr@gmail.com>
2026-07-17 07:51:09 +0000

    Maths-round-floor-ceil-random

--- 01_BASICS/06_nums_and_math.js
+++ 01_BASICS/06_nums_and_math.js
@@ -1,23 +1,58 @@
 const score = 500
-console.log(score);
+//console.log(score);
 
 
 
 const balance =new Number(400)
-console.log(balance);
+//console.log(balance);
 
 
-console.log(balance.toString().length);
-console.log(balance.toFixed(2));   //Precision  value
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));   //Precision  value
 
 
 const otherNumber =23.8966
-console.log(otherNumber.toPrecision(3)); ///start from 23.9  
+//console.log(otherNumber.toPrecision(3)); ///start from 23.9  
 
 
 
 const hundreds=1000000
-console.log(hundreds.toLocaleString('en-IN'));
+//console.log(hundreds.toLocaleString('en-IN'));
+
+
+//+++++++++++++++++++++++MATHS++++++++++++++++++++++++++++++++++++++++
+
+
+console.log(Math);
+
+console.log(Math.abs(-4));   
+console.log(Math.round(4.3));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+
+console.log(Math.max(23,45,67,89,545));
+console.log(Math.min(456,78,43,3,67,67344));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) +1);
+
+
+
+const min=10
+const max=20
+
+
+
+console.log(Math.floor(Math.random() *(max-min+1)+min));
+
+
+
+
+
+
+
+
 
 
 
