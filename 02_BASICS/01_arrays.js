9ae9671d9179efd79997d0e1c2a24ba8ab48ede2
aistacker <lionsvkr@gmail.com>
2026-07-17 09:03:51 +0000

    Arrays

---
+++ 02_BASICS/01_arrays.js
@@ -0,0 +1,51 @@
+//Array
+
+const myArr = [0,45,67,23,56,78,233,67]
+
+const myArr2=["abc","fgtgf","trrt","rgrth"]
+
+const myArr3=new Array(1,45,67,78,90,8)
+
+//console.log(myArr[1]);
+
+// Array Methods
+
+//myArr.push(6)
+//myArr.push(7)
+//myArr.pop(7)
+
+//myArr.unshift(0)
+//myArr.shift()
+
+//console.log(myArr.includes(9));
+
+//console.log(myArr.indexOf(9));
+//console.log(myArr);
+//onst newArr= myArr.join()
+
+//console.log(myArr);
+//console.log(typeof newArr);
+
+
+//slice ,splice
+
+console.log("A ",myArr);
+const myn1 =myArr.slice(1,3)
+
+console.log(myn1);
+
+console.log("B ",myArr);
+const myn2 =myArr.splice(1,3)
+
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
+
